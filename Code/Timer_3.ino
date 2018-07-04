90fc3f08770094c17ad8e4ff1dec7a95f24969de
Stratos Gkagkanis <33279890+StratosGK@users.noreply.github.com>
2018-07-04 12:16:01 +0300

    Rename Timer_3.ino to Code/Timer_3.ino

---
+++ Code/Timer_3.ino
@@ -0,0 +1,40 @@
+/*
+ * Timer 3
+ *    by Stratos Gkagkanis
+ * 16bit Timer 3 is used to count the sampling period.
+ */
+void T3Setup(){
+  TCNT3 = 0;
+  TCCR3A = 0;
+  TCCR3B = 0;
+  ICR3 = 0;
+  OCR3A = 0;
+  OCR3B = 0;
+  TIMSK3 = 0;
+  TIFR3 = 0;
+  
+  TCCR3B = (1<<WGM32);    //Mode 4 => CTC, TOP = OCR1A
+  
+  OCR3A = 4999;                     //Compare Value for 20ms
+  TIMSK3 = 2;                       //OCIEA = 1
+  TCCR3B |= (1<<CS31 | 1<<CS30);    //Prescaler = 64, Time = 62.5ns * 64 * 5000 = 20ms.
+  //T3 is now ON
+}
+
+ISR(TIMER3_COMPA_vect){
+  T3Flag = true;
+}
+
+void T3Start(){
+  TCCR3B |= (1<<CS31 | 1<<CS30);  //Prescaler = 64, Opote: xronos = 62.5ns * 64 * 5000 = 20ms.
+}
+
+void T3Stop(){
+  TCCR3B = (1<<WGM32);
+  TCNT3 = 0;
+}
+
+void T3Pause(){
+   TCCR3B = (1<<WGM32);
+}
+
